b40eecd815d3464295f1721aa0a1071139626eae
utcarnivaldayo <ut091210.0495@gmail.com>
2022-09-04 02:43:01 +0900

    Add test clear and upper, lower, add_bulk

--- curve_statistics/src/curve_statistics.rs
+++ curve_statistics/src/curve_statistics.rs
@@ -1,113 +1,151 @@
 use incremental_statistics::incremental_statistics;
 
 pub struct CurveStatistics {
-    points: Vec<incremental_statistics::IncrementalStatistics>,
+    series: Vec<incremental_statistics::IncrementalStatistics>,
     count: usize,
 }
 
 impl CurveStatistics {
     pub fn new(length: usize) -> CurveStatistics {
         CurveStatistics {
-            points: vec![incremental_statistics::IncrementalStatistics::new(); length],
+            series: vec![incremental_statistics::IncrementalStatistics::new(); length],
             count: 0usize,
         }
     }
 
     pub fn mean(&self, index: usize) -> f64 {
-        self.points[index].mean()
+        self.series[index].mean()
     }
 
     pub fn means(&self) -> Vec<f64> {
-        let mut means: Vec<f64> = vec![0.0; points.len()];
-        for i in 0..self.points.len() {
-            means[i] = self.points[i].mean();
+        let mut means: Vec<f64> = vec![0.0; self.series.len()];
+        for i in 0..self.series.len() {
+            means[i] = self.series[i].mean();
         }
         means
     }
 
     pub fn variance(&self, index: usize) -> f64 {
-        self.points[index].variance()
+        self.series[index].variance()
     }
 
     pub fn variances(&self) -> Vec<f64> {
-        let mut variances: Vec<f64> = vec![0.0; points.len()];
-        for i in 0..self.points.len() {
-            variances[i] = self.points[i].variance();
+        let mut variances: Vec<f64> = vec![0.0; self.series.len()];
+        for i in 0..self.series.len() {
+            variances[i] = self.series[i].variance();
         }
         variances
     }
 
     pub fn un_variance(&self, index: usize) -> f64 {
-        self.points[index].un_variance()
+        self.series[index].un_variance()
     }
 
     pub fn un_variances(&self) -> Vec<f64> {
-        let mut un_variances: Vec<f64> = vec![0.0; points.len()];
-        for i in 0..self.points.len() {
-            un_variances[i] = self.points[i].un_variance();
+        let mut un_variances: Vec<f64> = vec![0.0; self.series.len()];
+        for i in 0..self.series.len() {
+            un_variances[i] = self.series[i].un_variance();
         }
         un_variances
     }
 
     pub fn standard_deviation(&mut self, index: usize) -> f64 {
-        self.points[i].standard_deviation()
+        self.series[index].standard_deviation()
     }
 
     pub fn standard_deviations(&mut self) -> Vec<f64> {
-        let mut standard_deviations: Vec<f64> = vec![0.0; points.len()];
-        for i in 0..self.points.len() {
-            standard_deviations[i] = self.points[i].un_standard_deviation();
+        let mut standard_deviations: Vec<f64> = vec![0.0; self.series.len()];
+        for i in 0..self.series.len() {
+            standard_deviations[i] = self.series[i].standard_deviation();
         }
         standard_deviations
     }
 
     pub fn un_standard_deviation(&mut self, index: usize) -> f64 {
-        self.points[index].un_standard_deviation()
+        self.series[index].un_standard_deviation()
     }
 
     pub fn un_standard_deviations(&mut self) -> Vec<f64> {
-        let mut un_standard_deviations: Vec<f64> = vec![0.0; points.len()];
-        for i in 0..self.points.len() {
-            un_standard_deviations[i] = self.points[i].un_standard_deviation();
+        let mut un_standard_deviations: Vec<f64> = vec![0.0; self.series.len()];
+        for i in 0..self.series.len() {
+            un_standard_deviations[i] = self.series[i].un_standard_deviation();
         }
         un_standard_deviations
     }
 
-    pub fn upper(&self, index: usize) -> f64 {
-        self.points[index].upper()
+    pub fn upper(&mut self, index: usize) -> f64 {
+        self.series[index].upper()
     }
 
     pub fn uppers(&mut self) -> Vec<f64> {
-        let mut uppers: Vec<f64> = vec![0.0; points.len()];
-        for i in 0..self.points.len() {
-            uppers[i] = self.points[i].upper();
+        let mut uppers: Vec<f64> = vec![0.0; self.series.len()];
+        for i in 0..self.series.len() {
+            uppers[i] = self.series[i].upper();
         }
         uppers
     }
 
-    pub fn lower(&mut self, index: usize) {
-        self.points[i].lower()
+    pub fn un_upper(&mut self, index: usize) -> f64 {
+        self.series[index].un_upper()
+    }
+
+    pub fn un_uppers(&mut self) -> Vec<f64> {
+        let mut un_uppers: Vec<f64> = vec![0.0; self.series.len()];
+        for i in 0..self.series.len() {
+            un_uppers[i] = self.series[i].un_upper();
+        }
+        un_uppers
+    }
+
+    pub fn lower(&mut self, index: usize) -> f64 {
+        self.series[index].lower()
     }
 
     pub fn lowers(&mut self) -> Vec<f64> {
-        let mut lowers: Vec<f64> = vec![0.0; points.len()];
-        for i in 0..self.points.len() {
-            lowers[i] = self.points[i].lower();
+        let mut lowers: Vec<f64> = vec![0.0; self.series.len()];
+        for i in 0..self.series.len() {
+            lowers[i] = self.series[i].lower();
         }
         lowers
     }
 
+    pub fn un_lower(&mut self, index: usize) -> f64 {
+        self.series[index].un_lower()
+    }
+
+    pub fn un_lowers(&mut self) -> Vec<f64> {
+        let mut un_lowers: Vec<f64> = vec![0.0; self.series.len()];
+        for i in 0..self.series.len() {
+            un_lowers[i] = self.series[i].un_lower();
+        }
+        un_lowers
+    }
+
     pub fn len(&self) -> usize {
-        self.points.len()
+        self.series.len()
     }
 
     pub fn count(&self) -> usize {
         self.count
     }
 
-    pub fn clear(&mut self) {
-        for i in 0..self.points.len() {
-            self.points[i].clear();
+    pub fn add(&mut self, series: &Vec<f64>) {
+        for i in 0..series.len() {
+            self.series[i].add(series[i]);
         }
+        self.count += 1;
+    }
+
+    pub fn add_bulk(&mut self, series_bulk: &Vec<Vec<f64>>) {
+        for i in series_bulk {
+            self.add(i);
+        }
+    }
+
+    pub fn clear(&mut self) {
+        for i in 0..self.series.len() {
+            self.series[i].clear();
+        }
+        self.count = 0;
     }
 }
--- curve_statistics/src/lib.rs
+++ curve_statistics/src/lib.rs
@@ -1,7 +1,183 @@
+pub mod curve_statistics;
+
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::curve_statistics;
 
     #[test]
-    fn it_works() {}
+    fn test_add() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        for i in 1..=length {
+            let arr: Vec<f64> = vec![i as f64; length];
+            cur.add(&arr);
+        }
+        assert_eq!(cur.count(), length);
+        assert_eq!(cur.len(), length);
+    }
+
+    #[test]
+    fn test_add_bulk() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        let matrix: Vec<Vec<f64>> = vec![
+            vec![1.0f64; length],
+            vec![2.0f64; length],
+            vec![3.0f64; length],
+            vec![4.0f64; length],
+            vec![5.0f64; length],
+        ];
+
+        cur.add_bulk(&matrix);
+        assert_eq!(cur.count(), length);
+        assert_eq!(cur.len(), length);
+        for i in 0..length {
+            assert!((cur.mean(i) - 3.0) < 1e-6);
+        }
+    }
+
+    #[test]
+    fn test_mean() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        for i in 1..=length {
+            let arr: Vec<f64> = vec![i as f64; length];
+            cur.add(&arr);
+        }
+        assert_eq!(cur.count(), length);
+
+        let means = cur.means();
+        for i in 0..length {
+            assert!((cur.mean(i) - 3.0).abs() < 1e-6);
+            assert!((means[i] - 3.0).abs() < 1e-6);
+        }
+    }
+
+    #[test]
+    fn test_standard_deviation() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        for i in 1..=length {
+            let arr: Vec<f64> = vec![i as f64; length];
+            cur.add(&arr);
+        }
+        assert_eq!(cur.count(), length);
+
+        let standard_deviations = cur.standard_deviations();
+        for i in 0..length {
+            assert!((cur.standard_deviation(i) - 2.0f64.sqrt()).abs() < 1e-6);
+            assert!((standard_deviations[i] - 2.0f64.sqrt()).abs() < 1e-6);
+        }
+    }
+
+    #[test]
+    fn test_un_standard_deviation() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        for i in 1..=length {
+            let arr: Vec<f64> = vec![i as f64; length];
+            cur.add(&arr);
+        }
+        assert_eq!(cur.count(), length);
+
+        let un_standard_deviations = cur.un_standard_deviations();
+        for i in 0..length {
+            assert!((cur.un_standard_deviation(i) - 2.5f64.sqrt()).abs() < 1e-6);
+            assert!((un_standard_deviations[i] - 2.5f64.sqrt()).abs() < 1e-6);
+        }
+    }
+
+    #[test]
+    fn test_upper() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        for i in 1..=length {
+            let arr: Vec<f64> = vec![i as f64; length];
+            cur.add(&arr);
+        }
+        assert_eq!(cur.count(), length);
+
+        let uppers = cur.uppers();
+        for i in 0..length {
+            assert!((cur.upper(i) - (cur.mean(i) + 2.0f64.sqrt())).abs() < 1e-6);
+            assert!((uppers[i] - (cur.mean(i) + 2.0f64.sqrt())).abs() < 1e-6);
+        }
+    }
+
+    #[test]
+    fn test_un_upper() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        for i in 1..=length {
+            let arr: Vec<f64> = vec![i as f64; length];
+            cur.add(&arr);
+        }
+        assert_eq!(cur.count(), length);
+
+        let un_uppers = cur.un_uppers();
+        for i in 0..length {
+            assert!((cur.un_upper(i) - (cur.mean(i) + 2.5f64.sqrt())).abs() < 1e-6);
+            assert!((un_uppers[i] - (cur.mean(i) + 2.5f64.sqrt())).abs() < 1e-6);
+        }
+    }
+
+    #[test]
+    fn test_lower() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        for i in 1..=length {
+            let arr: Vec<f64> = vec![i as f64; length];
+            cur.add(&arr);
+        }
+        assert_eq!(cur.count(), length);
+
+        let lowers = cur.lowers();
+        for i in 0..length {
+            assert!((cur.lower(i) - (cur.mean(i) - 2.0f64.sqrt())).abs() < 1e-6);
+            assert!((lowers[i] - (cur.mean(i) - 2.0f64.sqrt())).abs() < 1e-6);
+        }
+    }
+
+    #[test]
+    fn test_un_lower() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        for i in 1..=length {
+            let arr: Vec<f64> = vec![i as f64; length];
+            cur.add(&arr);
+        }
+        assert_eq!(cur.count(), length);
+
+        let un_lowers = cur.un_lowers();
+        for i in 0..length {
+            assert!((cur.un_lower(i) - (cur.mean(i) - 2.5f64.sqrt())).abs() < 1e-6);
+            assert!((un_lowers[i] - (cur.mean(i) - 2.5f64.sqrt())).abs() < 1e-6);
+        }
+    }
+
+    #[test]
+    fn test_clear() {
+        let length: usize = 5;
+        let mut cur = curve_statistics::CurveStatistics::new(length);
+
+        for i in 1..=length {
+            let arr: Vec<f64> = vec![i as f64; length];
+            cur.add(&arr);
+        }
+        assert_eq!(cur.count(), length);
+        cur.clear();
+        assert_eq!(cur.count(), 0usize);
+        for i in 0..length {
+            assert!(cur.mean(i) < 1e-6);
+        }
+    }
 }
